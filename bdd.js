c965dd45529a62c2fd97972d5685d42adc9dc206
Valentin <Alencruserxx@hotmail.fr>
2018-11-07 16:57:09 +0100

    suppression de bdd

--- bdd.js
+++
@@ -1,8 +0,0 @@
-let mysql = require('mysql');
-let connection = mysql.createConnection({
-	host: 'den1.mysql1.gear.host',
-	user: 'simplonmi',
-	password: 'Jr2h4jQY-P~r',
-	database: 'simplonmi'
-});
-module.exports= connection;
